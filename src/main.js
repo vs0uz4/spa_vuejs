3cfca6db260433bfd305fa3c90193e0b42a20b95
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-12-03 19:14:35 -0200

    Refactored main.js and Create new Container
    - Refactored main.js for register new container TheContainer;
    - Created TheContainer;
    - Created TheNavigation.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import TheContainer from './the-container'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -10,6 +8,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  template: '<the-container/>',
+  components: { TheContainer }
 })
