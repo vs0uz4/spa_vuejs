763d8f0f8053fb0c725ca99b685f59a75ba2d608
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-12-06 20:03:56 -0200

    Installed jQuery and Bootstrap Javascripts

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+window.$ = window.jQuery = require('jquery')
+require('bootstrap-sass')
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
