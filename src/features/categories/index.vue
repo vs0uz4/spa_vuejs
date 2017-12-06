ef4d4c8103c484eb4da69619890c7d3dddfcec5f
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-12-06 19:05:13 -0200

    Implemented method for get categories with axios in feature/categories
    - Get token from storage with localforage;
    - Imported axios and configured token in headers;
    - Get categories from API;
    - Implemented errorHandling and sucessHandling.

--- src/features/categories/index.vue
+++ src/features/categories/index.vue
@@ -1,11 +1,39 @@
 <script>
+  import axios from 'axios'
+  import localforage from 'localforage'
+
   export default {
-    name: 'Categories'
+    name: 'Categories',
+    data () {
+      return {
+        list: []
+      }
+    },
+    async mounted () {
+      const url = 'http://localhost:3456/categoria'
+      const token = await localforage.getItem('token')
+
+      axios.defaults.headers.common['x-access-token'] = token
+      axios.get(url)
+        .then(this.successHandler)
+        .catch(this.errorHandler)
+    },
+    methods: {
+      successHandler (res) {
+        if (res != null) {
+          this.list = res.data.categories
+        }
+      },
+      errorHandler (error) {
+        console.log(error, error.message)
+      }
+    }
   }
 </script>
 
 <template>
   <div>
     <h1>Sou o Componente Categories.index</h1>
+    <p v-for="category in list" :key="category.id">{{ category.name }}</p>
   </div>
 </template>
