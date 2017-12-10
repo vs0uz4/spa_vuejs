7ad2b1bb0caeba82debf54c79c88a0ee24fbbe9a
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-12-10 19:06:59 -0200

    Installed and Configured vuelidate

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import TheContainer from './the-container'
 import router from './router'
 import eventBus from './plugins/event-bus'
@@ -7,6 +8,8 @@ window.$ = window.jQuery = require('jquery')
 require('bootstrap-sass')
 
 Vue.config.productionTip = false
+
+Vue.use(Vuelidate)
 Vue.use(eventBus)
 
 /* eslint-disable no-new */
