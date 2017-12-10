c3eeaa03345dfe1db44ef4ad8a2841489e43c123
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-12-10 09:26:59 -0200

    Refactor alerts component for hide after timeout

--- src/components/alerts.vue
+++ src/components/alerts.vue
@@ -20,10 +20,25 @@
     },
     computed: {
       shouldDisplaySuccessMessage () {
-        return this.messages.success !== ''
+        const condicao = this.messages.success !== ''
+
+        if (condicao) {
+          setTimeout(() => {
+            this.messages.success = ''
+          }, 4000)
+        }
+        return condicao
       },
       shouldDisplayErrorMessage () {
-        return this.messages.error !== ''
+        const condicao = this.messages.error !== ''
+
+        if (condicao) {
+          setTimeout(() => {
+            this.messages.error = ''
+          }, 4000)
+        }
+
+        return condicao
       }
     },
     methods: {
@@ -37,7 +52,7 @@
 <template>
   <div>
     <div v-show="shouldDisplaySuccessMessage" class="alert alert-success alert-dismissible" role="alert">
-      <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+      <span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>
       <span class="sr-only">Success:</span>
       <button @click="close('success')" class="close" type="button" aria-label="Close">
         <span aria-hidden="true">&times;</span>
