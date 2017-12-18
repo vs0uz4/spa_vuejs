50060e561a2fc6513d96e68846b285047b3edf8f
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-12-18 00:35:50 -0200

    Implement in HTTP Service Interceptors setSearching

--- src/service/http.js
+++ src/service/http.js
@@ -1,23 +1,33 @@
 import axios from 'axios'
 import localforage from 'localforage'
 import router from '@/router'
+import store from '@/store'
 import { bus } from '@/plugins/event-bus'
 
 const http = axios.create({
   baseURL: process.env.API_URL
 })
 
+const setSearching = (state) => {
+  store.dispatch('setSearching', { isSearching: state })
+}
+
 const interceptRequest = async (config) => {
+  setSearching(true)
   const token = await localforage.getItem('token')
   config.headers.common['x-access-token'] = token
   return config
 }
 
 const interceptRequestError = (error) => {
+  setSearching(false)
   Promise.reject(error)
 }
 
-const interceptResponse = (response) => response
+const interceptResponse = (response) => {
+  setSearching(false)
+  return response
+}
 
 const interceptResponseError = (error) => {
   let message = error.message
@@ -39,6 +49,7 @@ const interceptResponseError = (error) => {
     message
   })
 
+  setSearching(false)
   Promise.reject(error)
 }
 
